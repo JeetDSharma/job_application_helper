22baa04bb26414debf95227223c77485f66e8644
JeetDSharma <jeetsharma2112@gmail.com>
2025-07-11 16:43:32 -0400

    formatted code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 import Image from "next/image";
-import React, { useState, useRef} from 'react';
-import toast, {Toaster} from 'react-hot-toast';
+import React, { useState, useRef } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function Home() {
   const [emailForm, setEmailForm] = useState({
@@ -9,131 +9,148 @@ export default function Home() {
     name: "",
     company: "",
     jobPosition: "",
-    isAlum: false
+    isAlum: false,
   });
   const timerRef = useRef<NodeJS.Timeout | null>(null);
-;
   const [isPending, setIsPending] = useState(false);
 
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  const { id, value, type, checked } = e.target;
+    const { id, value, type, checked } = e.target;
 
-  setEmailForm(prev => ({
-    ...prev,
-    [id]: type === 'checkbox' ? checked : value,
-  }));
-};
+    setEmailForm((prev) => ({
+      ...prev,
+      [id]: type === "checkbox" ? checked : value,
+    }));
+  };
 
+  const handleSubmit = async () => {
+    if (isPending) return;
+    setIsPending(true);
 
- const handleSubmit = async () => {
-  if (isPending) return; 
-  setIsPending(true);
+    toast(
+      (t) => (
+        <span>
+          Sending Email ....
+          <button
+            onClick={() => {
+              if (timerRef.current) {
+                clearTimeout(timerRef.current);
+                timerRef.current = null;
+              }
+              toast.dismiss(t.id);
+              toast("Email sending cancelled.");
+              setIsPending(false);
+            }}
+            className="ml-4 text-red-500 underline"
+          >
+            Click To Cancel
+          </button>
+        </span>
+      ),
+      { duration: 10000 }
+    );
 
-  toast(
-  (t) => (
-    <span>
-      Sending in 10 seconds...
-      <button
-        onClick={() => {
-          if (timerRef.current) {
-            clearTimeout(timerRef.current);
-            timerRef.current = null;
-          }
-          toast.dismiss(t.id);
-          toast("Email sending cancelled.");
-          setIsPending(false);
-        }}
-        className="ml-4 text-red-500 underline"
-      >
-        Cancel
-      </button>
-    </span>
-  ),
-  { duration: 10000 }
-);
+    timerRef.current = setTimeout(async () => {
+      try {
+        const response = await fetch("/api/send-email", {
+          headers: { Accept: "application/json" },
+          method: "POST",
+          body: JSON.stringify({ ...emailForm }),
+        });
 
-
-  timerRef.current = setTimeout(async () => {
-  try {
-    const response = await fetch("/api/send-email", {
-      headers: { Accept: "application/json" },
-      method: "POST",
-      body: JSON.stringify({ ...emailForm }),
-    });
-
-    const data = await response.json();
-    if (response.ok) {
-      toast.success("Email Sent Successfully!");
-    } else {
-      console.error(`Failed to send email: ${data.error || "Unknown error"}`);
-      toast.error("Failed to Send Email");
-    }
-  } catch (error) {
-    console.error("Send email error:", error);
-    toast.error("Failed to Send Email");
-  } finally {
-    setIsPending(false);
-    timerRef.current = null;
-  }
-},10000);
-
-
-};
+        const data = await response.json();
+        if (response.ok) {
+          toast.success("Email Sent Successfully!");
+        } else {
+          console.error(
+            `Failed to send email: ${data.error || "Unknown error"}`
+          );
+          toast.error("Failed to Send Email");
+        }
+      } catch (error) {
+        console.error("Send email error:", error);
+        toast.error("Failed to Send Email");
+      } finally {
+        setIsPending(false);
+        timerRef.current = null;
+      }
+    }, 10000);
+  };
 
   return (
     <>
-    
-    <Toaster position="top-center" reverseOrder={false} />
-    <div className="min-h-screen flex justify-center items-center bg-gray-100">
-      <div className="bg-white p-8 shadow-md rounded-lg w-lg">
-        <h1 className="text-2xl font-semibold text-center mb-6">Send Email</h1>
-        <form className="flex flex-col gap-4" onSubmit={(e)=>{
-          e.preventDefault(); handleSubmit();
-        }}>
-          <div className="flex flex-col">
-
-          <label htmlFor="email" className="text-sm font-medium text-gray-700">Email</label>
-          <input
-            type="email"
-            name = "email"
-            id="email"
-            onChange={handleChange}
-            className="border border-gray-300 mt-1 px-3 py-2 rounded-md focus:outline-none focus:ring focus:ring-indigo-500" 
-            />
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className="min-h-screen flex justify-center items-center bg-gray-100">
+        <div className="bg-white p-8 shadow-md rounded-lg w-lg">
+          <h1 className="text-2xl font-semibold text-center mb-6">
+            Send Email
+          </h1>
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+          >
+            <div className="flex flex-col">
+              <label
+                htmlFor="email"
+                className="text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                onChange={handleChange}
+                className="border border-gray-300 mt-1 px-3 py-2 rounded-md focus:outline-none focus:ring focus:ring-indigo-500"
+              />
             </div>
             <div className="flex flex-col">
-
-          <label htmlFor="company" className="text-sm font-medium text-gray-700">Company Name</label>
-          <input
-            type="text"
-            name="company"
-            id="company"
-            onChange={handleChange}
-            className="border border-gray-300 mt-1 px-3 py-2 rounded-md focus:outline-none focus:ring focus:ring-indigo-500" 
-            />
+              <label
+                htmlFor="company"
+                className="text-sm font-medium text-gray-700"
+              >
+                Company Name
+              </label>
+              <input
+                type="text"
+                name="company"
+                id="company"
+                onChange={handleChange}
+                className="border border-gray-300 mt-1 px-3 py-2 rounded-md focus:outline-none focus:ring focus:ring-indigo-500"
+              />
             </div>
             <div className="flex flex-col">
-
-          <label htmlFor="name" className="text-sm font-medium text-gray-700">Name</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            onChange={handleChange}
-            className="border border-gray-300 mt-1 px-3 py-2 rounded-md focus:outline-none focus:ring focus:ring-indigo-500" 
-            />
+              <label
+                htmlFor="name"
+                className="text-sm font-medium text-gray-700"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                onChange={handleChange}
+                className="border border-gray-300 mt-1 px-3 py-2 rounded-md focus:outline-none focus:ring focus:ring-indigo-500"
+              />
             </div>
             <div className="flex flex-col">
-
-          <label htmlFor="jobPosition" className="text-sm font-medium text-gray-700">Job Position</label>
-          <input
-            type="text"
-            id="jobPosition"
-            name="jobPosition"
-            onChange={handleChange}
-            className="border border-gray-300 mt-1 px-3 py-2 rounded-md focus:outline-none focus:ring focus:ring-indigo-500" 
-            />
+              <label
+                htmlFor="jobPosition"
+                className="text-sm font-medium text-gray-700"
+              >
+                Job Position
+              </label>
+              <input
+                type="text"
+                id="jobPosition"
+                name="jobPosition"
+                onChange={handleChange}
+                className="border border-gray-300 mt-1 px-3 py-2 rounded-md focus:outline-none focus:ring focus:ring-indigo-500"
+              />
             </div>
             <div className="flex items-center gap-2">
               <input
@@ -142,13 +159,18 @@ export default function Home() {
                 checked={emailForm.isAlum}
                 onChange={handleChange}
                 className=""
-                />
-               <label className="text-sm"> Is Alum?</label>
+              />
+              <label className="text-sm"> Is Alum?</label>
             </div>
-          <button type="submit" className="border border-black rounded-md px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-white">Send Email</button>
-        </form>
+            <button
+              type="submit"
+              className="border border-black rounded-md px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-white"
+            >
+              Send Email
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
     </>
   );
 }
