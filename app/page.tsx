55fb00cc42584f3a7fe6e36cd474c4e1c7267a85
JeetDSharma <jeetsharma2112@gmail.com>
2025-07-11 00:34:29 -0400

    updated form css

--- app/page.tsx
+++ app/page.tsx
@@ -53,51 +53,65 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <h1>Email Sender</h1>
-      <div className="">
-        <form className="flex flex-row w-xl px-8 py-4 mx-4">
+    <div className="min-h-screen flex justify-center items-center bg-gray-100">
+      <div className="bg-white p-8 shadow-md rounded-lg w-lg">
+        <h1 className="text-2xl font-semibold text-center mb-6">Send Email</h1>
+        <form className="flex flex-col gap-4">
+          <div className="flex flex-col">
+
+          <label htmlFor="email" className="text-sm font-medium text-gray-700">Email</label>
           <input
-            type="text"
-            placeholder="email"
+            type="email"
             name = "email"
             id="email"
             onChange={handleChange}
-            className="border border-black p-2 ml-4"
-          />
+            className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 placeholder-gray-400 transition"
+            />
+            </div>
+            <div className="flex flex-col">
+
+          <label htmlFor="company" className="text-sm font-medium text-gray-700">Company Name</label>
           <input
             type="text"
-            placeholder="company"
             name="company"
             id="company"
             onChange={handleChange}
-            className="border border-black p-2 ml-4"
-          />
+            className="border border-gray-300 mt-1 px-3 py-2 rounded-md focus:outline-none focus:ring focus:ring-indigo-500" 
+            />
+            </div>
+            <div className="flex flex-col">
+
+          <label htmlFor="name" className="text-sm font-medium text-gray-700">Name</label>
           <input
             type="text"
-            placeholder="name"
             name="name"
             id="name"
             onChange={handleChange}
-            className="border border-black p-2 ml-4"
-          />
+            className="border border-gray-300 mt-1 px-3 py-2 rounded-md focus:outline-none focus:ring focus:ring-indigo-500" 
+            />
+            </div>
+            <div className="flex flex-col">
+
+          <label htmlFor="job_position" className="text-sm font-medium text-gray-700">Job Position</label>
           <input
             type="text"
-            placeholder="Job Position"
             id="job_position"
             name="job_position"
             onChange={handleChange}
-            className="border border-black p-2 ml-4"
-          />
-          <label> <input
-            type="checkbox"
-            id="isAlum"
-            checked={emailForm.isAlum}
-            onChange={handleChange}
-            className="h-4 w-4 ml-4"
-          />
-          <span>Is Alum?</span></label>
-          <button type="button"  onClick={handleSubmit} className="border border-black py-2 px-4 rounded ml-4">Send Email</button>
+            className="border border-gray-300 mt-1 px-3 py-2 rounded-md focus:outline-none focus:ring focus:ring-indigo-500" 
+            />
+            </div>
+            <div className="flex items-center gap-2">
+              <input
+                type="checkbox"
+                id="isAlum"
+                checked={emailForm.isAlum}
+                onChange={handleChange}
+                className=""
+                />
+               <label className="text-sm"> Is Alum?</label>
+            </div>
+          <button type="submit"  onClick={handleSubmit} className="border border-black rounded-md px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-white">Send Email</button>
         </form>
       </div>
     </div>
